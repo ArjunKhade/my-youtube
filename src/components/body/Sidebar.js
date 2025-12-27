13618dbbd8894d85111e7c86d72fe9ecbbae8ccf
DESKTOP-OSI02RS\khade <khadearjun@gmail.com>
2025-12-27 13:08:28 +0530

    created component body, side bar,  and main container

---
+++ src/components/body/Sidebar.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="w-52 shadow-lg p-8">
+
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Video</li>
+        <li>Live</li>
+      </ul>
+      <h1 className="font-bold pt-6">Subscription</h1>
+      <ul>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+      </ul>
+
+      <h1 className="font-bold pt-6">Watch Later</h1>
+      <ul>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+        <li>Music</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
