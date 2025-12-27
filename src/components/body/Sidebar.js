2c1a386d3ba1788ffad03ea39209ca3f1858c393
DESKTOP-OSI02RS\khade <khadearjun@gmail.com>
2025-12-27 17:23:47 +0530

    dispatch action on hamberger button clicked. read the value from store to show and hide the side bar

--- src/components/body/Sidebar.js
+++ src/components/body/Sidebar.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+
+const isMenuOpen = useSelector(store => store.app.isMenuOpen);
+
+if(!isMenuOpen) return;
+
   return (
     <div className="w-52 shadow-lg p-8">
 
