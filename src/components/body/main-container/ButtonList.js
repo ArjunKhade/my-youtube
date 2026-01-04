ce2f8cbd281b6b5930fcd2a4295656f924e0adfa
Arjun <khadearjun@gmail.com>
2026-01-04 17:53:48 +0530

    updated button list compoent

--- src/components/body/main-container/ButtonList.js
+++ src/components/body/main-container/ButtonList.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Button from './Button'
 
 const ButtonList = () => {
+  const buttonNames = ["All", "Songs", "Gaming", "Cricket", "Movies", "Food", "Cooking", "News", "Sports", "Technology", "Comedy", "Travel", "Education"];
+
   return (
-    <div>ButtonList</div>
+    <div className='flex flex-wrap'>
+      {buttonNames.map((name, index) => (
+        <Button key={index} name={name} />
+      ))}
+    </div>
   )
 }
 
