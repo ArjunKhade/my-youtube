688a2d284ed13255cf978cf5ee7578ada0a422d2
Arjun <khadearjun@gmail.com>
2026-01-07 22:03:06 +0530

    used suggetion query from the store

--- src/components/body/main-container/VideoContainer.js
+++ src/components/body/main-container/VideoContainer.js
@@ -2,17 +2,20 @@ import React, { useEffect, useState } from 'react'
 import { YOUTUBE_API } from '../../../constant/constant';
 import VideoCard from './VideoCard';
 import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
 
 const VideoContainer = () => {
 
   const [videos, setVideos]= useState([]);
 
+  const suggetion = useSelector((store) => store.suggetion.query)
+
   useEffect(() =>{
     getVideos();
-  },[])
+  },[suggetion])
 
   const getVideos = async ()=> {
-   const data = await fetch(YOUTUBE_API);
+   const data = await fetch(YOUTUBE_API(suggetion));
    const json = await data.json();
   //  console.log(json);
    setVideos(json.items);
