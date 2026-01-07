8b5023e39954c3c4be87a300fc0c82ddcbe9e41b
Arjun <khadearjun@gmail.com>
2026-01-07 22:04:34 +0530

    updated store to add suggetion

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,14 @@ import { configureStore } from "@reduxjs/toolkit"
 import AppSlice from "./appSlice";
 import SearchSlice from "./searchSlice";
 import ChatSlice from "./chatSlice";
-
+import SuggetionSlice from "./suggestionSlice"
 
 const store = configureStore({
   reducer: {
     app: AppSlice,
     search: SearchSlice,
-    chat: ChatSlice 
+    chat: ChatSlice,
+    suggetion: SuggetionSlice
   }
 })
 
