33dfbccdaa50bccaa377434871ca2cfbc74884de
Arjun <khadearjun@gmail.com>
2026-02-07 11:22:11 +0530

    added youtube api and youtube search suggestion api

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -2,3 +2,10 @@ export const LOGO = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_gWZ
 export const MENU = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN1wWjzGD7bfaqxEELh-qOwQYIG-zbaptuhg&s';
 export const USER_AVATAR = 'https://images.icon-icons.com/1378/PNG/512/avatardefault_92824.png';
 export const SEARCH = 'search.svg';
+
+export const YOUTUBE_API = (suggetion) =>  'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q='+suggetion+'&key='+ YOUTUBE_API_KEY
+
+export const YOUTUBE_SEARCH_API = (query) => {
+    return 'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='+query;
+}
+
